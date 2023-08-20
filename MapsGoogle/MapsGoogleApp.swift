4f556fc95f7ed96cabb56a41cef9973291dddd09
Tom Montgomery <montgomery3681@gmail.com>
2023-08-20 17:23:53 -0600

    Initial Commit

---
+++ MapsGoogle/MapsGoogleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapsGoogleApp.swift
+//  MapsGoogle
+//
+//  Created by tom montgomery on 8/20/23.
+//
+
+import SwiftUI
+
+@main
+struct MapsGoogleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
