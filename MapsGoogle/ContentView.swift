c477e3e23515b248067e9e703dacf752d9ac2007
Tom Montgomery <montgomery3681@gmail.com>
2024-01-01 21:48:06 -0700

    - open google maps, sample app

--- MapsGoogle/ContentView.swift
+++ MapsGoogle/ContentView.swift
@@ -7,15 +7,34 @@
 
 import SwiftUI
 
+// GOOGLE MAPS
+// 1- info.plist > add LSApplicationQueriesSchemes (now Queried URL Schemes)
+// a type of Launch Service Key.  Provides support for launching apps, matching doc types
+// 2- Add Row >> Item 0 = comgooglemaps
+
+
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Button("Open Maps") {
+            let latitude = 7.065
+            let longitude = 124.6
+            
+            // starts with comgooglemaps instead of HTTPS
+            let url = URL(string: "comgooglemaps://?saddr=&daddr=\(latitude),\(longitude)&directionsmode=driving")
+            
+            if UIApplication.shared.canOpenURL(url!) {
+                UIApplication.shared.open(url!, options: [:], completionHandler: nil)
+            } else {
+                // user does not have google maps installed
+                let newUrl = URL(string: "https://www.google.co.in/maps/dir/??saddr=\(latitude),\(longitude)&directionsmode=driving")
+                print("Opening newURL")
+                let superNewUrl = URL(string: "https://www.google.com/maps/place/QUINCE+COFFEE+HOUSE/@39.7390323,-104.9038455")
+                if UIApplication.shared.canOpenURL(superNewUrl!) {
+                    UIApplication.shared.open(superNewUrl!, options: [:], completionHandler: nil)
+                }
+            }
         }
-        .padding()
     }
 }
 
